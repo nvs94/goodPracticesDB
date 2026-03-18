022e430df36ec89a192e0683216e2993ed097a84
nvs94 <74451716+nvs94@users.noreply.github.com>
2026-03-18 12:38:23 +0100

    Update and rename helpers_utils.py to helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,15 +2,15 @@ from datetime import datetime, timedelta
 import itertools
 
 def get_previous_day(ts, frmt_in="%Y-%m-%d %H:%M:%S", frmt_out="%Y-%m-%d 00:00:00"):
-    """Devuelve el día anterior a un timestamp dado"""
+    """Calcula la fecha del día anterior a partir de un timestamp."""
     return (datetime.strptime(ts, frmt_in) - timedelta(days=1)).strftime(frmt_out)
 
 def unix_to_utc_string(unix_timestamp):
-    """Convierte timestamp Unix a string UTC"""
+    """Convierte timestamp Unix a string UTC."""
     return datetime.utcfromtimestamp(unix_timestamp).strftime("%Y-%m-%d %H:%M:%S")
 
 def pairwise(iterable):
-    """Genera pares consecutivos de un iterable"""
+    """Genera pares consecutivos de elementos de un iterable."""
     a, b = itertools.tee(iterable)
     next(b, None)
     return zip(a, b)
